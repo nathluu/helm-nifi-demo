7ad25f16f62a230943b2d9f90366fb2061f2b7a0
Tan Luu <luuhoangtanbis@gmail.com>
2024-03-01 22:00:57 +0700

    Fix deployment issue

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 
-ISTIO_REQUIRED="no"
+ISTIO_REQUIRED="yes"
+DEPLOY_NAMESPACE="nifi-demo"
 
 require() {
     hash kubectl 2>/dev/null || { echo >&2 "kubectl is not installed.  Aborting."; exit 1; }
@@ -14,28 +16,47 @@ installed_istio_version() {
 }
 
 install_istio() {
-    ./istio.sh
+    bash istio.sh
 }
 
-install() {
+install_cert_manager() {
+    helm repo add jetstack https://charts.jetstack.io
+    # Update your local Helm chart repository cache
+    helm repo update
+
+    # Install the cert-manager Helm chart
+    helm install \
+    cert-manager jetstack/cert-manager \
+    --namespace cert-manager \
+    --create-namespace \
+    --version v1.14.3 \
+    --set installCRDs=true
+}
+
+install_nifi() {
     local name="${1:?name is required}"
     local chartPath="${2:?chartPath is required}"
     local valueFilePath="${3:?valueFilePath is required}"
-    # pushd .
-    # cd $chartPath
-    # helm repo add bitnami https://charts.bitnami.com/bitnami
-    # helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-    # helm repo add dysnix https://dysnix.github.io/charts/
-    # helm repo add helm-repo https://charts.helm.sh/stable
-    # helm repo update
-    # helm dep up
-    # popd
-    result=$(kubectl get ns | grep "nifi-ns")
-    if [ "x$result" = "x" ]; then
-        kubectl create ns nifi-ns
-        kubectl label namespace nifi-ns istio-injection=enabled
+    pushd .
+    cd $chartPath
+    helm repo add bitnami https://charts.bitnami.com/bitnami
+    helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+    helm repo add dysnix https://dysnix.github.io/charts/
+    helm repo add helm-repo https://charts.helm.sh/stable
+    helm repo update
+    helm dep up
+    popd
+
+    if kubectl get namespace "$DEPLOY_NAMESPACE" > /dev/null 2>&1; then
+        echo "Namespace $DEPLOY_NAMESPACE exists."
+    else
+        echo "Namespace $DEPLOY_NAMESPACE does not exist. Creating ..."
+        kubectl create ns $DEPLOY_NAMESPACE
+        kubectl label namespace $DEPLOY_NAMESPACE istio-injection=enabled
     fi
-    helm install -f $valueFilePath $name $chartPath -n nifi-ns
+
+
+    helm install -f $valueFilePath $name $chartPath -n $DEPLOY_NAMESPACE
 
 }
 
@@ -44,5 +65,14 @@ istio_version=$(installed_istio_version)
 if [ "x$istio_version" = "x" -a "$ISTIO_REQUIRED" = "yes" ]; then
     install_istio
 fi
+
+if kubectl get namespace cert-manager > /dev/null 2>&1; then
+    echo "Namespace cert-manager exists."
+else
+    echo "Installing cert-manager ..."
+    install_cert_manager
+fi
+
+
 # install "$@"
-install nifi-release ./helm-nifi values-zk.yml
+install_nifi nifi-release ./helm-nifi values-oidc.yml
